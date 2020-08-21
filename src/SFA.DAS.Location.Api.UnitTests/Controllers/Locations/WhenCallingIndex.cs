788fc57ea5442f2d0e6d7e3f7fd12d43b7874512
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-21 12:55:41 +0100

    Implement controller action

--- src/SFA.DAS.Location.Api.UnitTests/Controllers/Locations/WhenCallingIndex.cs
+++ src/SFA.DAS.Location.Api.UnitTests/Controllers/Locations/WhenCallingIndex.cs
@@ -1,21 +1,61 @@
+using System;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
 using FluentAssertions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
+using SFA.DAS.Location.Api.ApiResponses;
 using SFA.DAS.Location.Api.Controllers;
+using SFA.DAS.Location.Application.Location.Queries;
+using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.Location.Api.UnitTests.Controllers.Locations
 {
     public class WhenCallingIndex
     {
-        [Test]
-        public void Then_An_Ok_Result_Is_Returned()
+        [Test, MoqAutoData]
+        public async Task Then_Gets_Locations_List_From_Mediator(
+            string query,
+            int results,
+            GetLocationsQueryResult queryResult,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] LocationsController controller)
         {
-            var controller = new LocationsController();
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.Is<GetLocationsQuery>(request => 
+                        request.Query == query && 
+                        request.ResultCount.Equals(results)), 
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(queryResult);
 
-            var actual = controller.Index() as ObjectResult;
-            
-            actual.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            var controllerResult = await controller.Index(query, results) as ObjectResult;
+
+            var model = controllerResult.Value as GetLocationsListResponse;
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            model.Locations.Should().BeEquivalentTo(queryResult.Locations, options=>options.ExcludingMissingMembers());
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_Exception_Then_Returns_Bad_Request(
+            string query,
+            int results,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] LocationsController controller)
+        {
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.IsAny<GetLocationsQuery>(),
+                    It.IsAny<CancellationToken>()))
+                .Throws<InvalidOperationException>();
+
+            var controllerResult = await controller.Index(query, results) as StatusCodeResult;
+
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.BadRequest);
         }
     }
 }
--- src/SFA.DAS.Location.Api/Controllers/LocationsController.cs
+++ src/SFA.DAS.Location.Api/Controllers/LocationsController.cs
@@ -1,4 +1,11 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Location.Api.ApiResponses;
+using SFA.DAS.Location.Application.Location.Queries;
 
 namespace SFA.DAS.Location.Api.Controllers
 {
@@ -7,11 +14,39 @@ namespace SFA.DAS.Location.Api.Controllers
     [Route("/api/[controller]/")]
     public class LocationsController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly ILogger<LocationsController> _logger;
+
+        public LocationsController (IMediator mediator, ILogger<LocationsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+        
         [HttpGet]
         [Route("")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index([FromQuery]string query, [FromQuery]int results)
         {
-            return Ok("ok");
+            try
+            {
+                var queryResult = await _mediator.Send(new GetLocationsQuery
+                {
+                    Query = query,
+                    ResultCount = results
+                });
+                
+                var response = new GetLocationsListResponse
+                {
+                    Locations = queryResult.Locations.Select(c=>(GetLocationsListItem)c).ToList()
+                };
+                
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e,$"Unable to get location data for {query} - number of results {results}");
+                return BadRequest();
+            }
         }
     }
 }
