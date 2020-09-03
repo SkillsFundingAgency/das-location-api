db7e720ed7ddd8ae068b8880764cf78297aa1567
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-09-03 10:35:21 +0100

    Write test for mapping Postcode API call to SuggestedLocation model

---
+++ src/SFA.DAS.Location.Domain.UnitTests/Models/WhenMappingPostcodeApiItemToSuggestedLocationModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using NUnit.Framework;
+using SFA.DAS.Location.Domain.ImportTypes;
+using SFA.DAS.Location.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Location.Domain.UnitTests.Models
+{
+    public class WhenMappingPostcodeApiItemToSuggestedLocationModel
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Correctly_Mapped(PostcodesLocationApiItem source)
+        {
+            var actual = (SuggestedLocation) source;
+            actual.Equals(source);
+        }
+    }
+}
