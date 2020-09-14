7beb0d7ddfef4d3796a34846b9f6f084fa90c994
Scott Wakefield <scott@expertcc.co.uk>
2020-09-14 18:58:18 +0100

    FAT2-189 removed dup types from api.

--- src/SFA.DAS.Location.Api/AppStart/HealthCheckStartup.cs
+++ src/SFA.DAS.Location.Api/AppStart/HealthCheckStartup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
-using SFA.DAS.Location.Api.Infrastructure;
+using SFA.DAS.Api.Common.Infrastructure;
 
 namespace SFA.DAS.Location.Api.AppStart
 {
