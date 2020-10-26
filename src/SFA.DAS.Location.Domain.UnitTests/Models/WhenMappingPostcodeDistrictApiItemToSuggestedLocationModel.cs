520c73a9d9ea5e6ddd9ef02c25d9d2819861bce7
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-10-26 13:50:38 +0000

    Write test to map PostcodeDistrictLocationApiResponse to SuggestedLocation

---
+++ src/SFA.DAS.Location.Domain.UnitTests/Models/WhenMappingPostcodeDistrictApiItemToSuggestedLocationModel.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Location.Domain.ImportTypes;
+using SFA.DAS.Location.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Location.Domain.UnitTests.Models
+{
+    public class WhenMappingPostcodeDistrictApiItemToSuggestedLocationModel
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Correctly_Mapped(PostcodeDistrictLocationApiResponse source)
+        {
+            //Arrange & Act
+            var actual = (SuggestedLocation)source;
+
+            //Assert
+            actual.AdminDistrict.Should().BeEquivalentTo(source.AdminDistrict[0]);
+            actual.Country.Should().BeEquivalentTo(source.Country[0]);
+
+            actual.Lat.Should().Be(source.Lat);
+            actual.Long.Should().Be(source.Long);
+            actual.Postcode.Should().Be(source.Postcode);
+            actual.Outcode.Should().Be(source.Outcode);
+
+
+
+        }
+    }
+}
