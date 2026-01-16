559c8d78b7582b74ab71ad388f1b228b98da5f86
killij <51908793+killij@users.noreply.github.com>
2026-01-16 14:34:11 +0000

    FAI-2975 Minor adjustment to OsPlacesUrlBuilder

--- src/SFA.DAS.Location.Infrastructure/ApiClient/OsPlacesUrlBuilder.cs
+++ src/SFA.DAS.Location.Infrastructure/ApiClient/OsPlacesUrlBuilder.cs
@@ -5,9 +5,12 @@ namespace SFA.DAS.Location.Infrastructure.ApiClient;
 
 public static class OsPlacesUrlBuilder
 {
+    private const string DefaultDataset = "dpa";
+    private const string DefaultSpatialReferenceSystem = "EPSG:4326";
+    private const string DefaultFilter = "COUNTRY_CODE:E";
     private const string OsPlacesPostCodeBaseUrl = "https://api.os.uk/search/places/v1/postcode";
     
-    public static string Create(string query, string dataset = "dpa", string outputSrs = "EPSG:4326", string filter = "COUNTRY_CODE:E", int? maxResults = null)
+    public static string Create(string query, string dataset = DefaultDataset, string outputSrs = DefaultSpatialReferenceSystem, string filter = DefaultFilter, int? maxResults = null)
     {
         var queryParams = new Dictionary<string, string>
         {
